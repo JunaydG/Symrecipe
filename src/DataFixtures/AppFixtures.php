8a67060fe6ad7baff217ab0d4a84df736fc4c0ca
Junayd.G <75118775+JunaydG@users.noreply.github.com>
2024-07-17 11:00:26 +0200

    💥 Add PaginatorBundle

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -19,13 +19,13 @@ class AppFixtures extends Fixture
     }
     public function load(ObjectManager $manager): void
     {
-        for ($i=0; $i <= 50 ; $i++) { 
+        for ($i=0; $i < 50 ; $i++) { 
             $ingredient = new Ingredient();
             $ingredient->setName($this->faker->word())
             ->setPrice(mt_rand(0, 100));
 
             $manager->persist($ingredient);
-        }
+        } 
 
      
         $manager->flush();
